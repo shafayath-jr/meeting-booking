f578c48e5fd4628d8f06b22fd87d4c7a0a7dd3c1
Hasibul Hamid <hasibul.c@penglobalbd.com>
2025-12-15 21:16:09 +0600

    feat: supabase setup

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { createClient } from "@/lib/supabase/server";
 
-function HomePage() {
-  return <Button>Submit</Button>;
+async function HomePage() {
+  const supabase = await createClient();
+  const { data: bookings } = await supabase.from("booking").select();
+
+  return (
+    <div className="">
+      <Button>Submit</Button>;
+    </div>
+  );
 }
 
 export default HomePage;
