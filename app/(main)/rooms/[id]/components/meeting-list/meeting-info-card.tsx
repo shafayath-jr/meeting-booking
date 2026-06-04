21de8bd3c71e9d9b4f4fefdd309c46325e9a6429
Shafayath Jamil Rafi <shafayath.r@penglobalbd.com>
2026-06-04 09:14:33 +0100

    Meeting Card Updated

--- app/(main)/rooms/[id]/components/meeting-list/meeting-info-card.tsx
+++ app/(main)/rooms/[id]/components/meeting-list/meeting-info-card.tsx
@@ -1,7 +1,7 @@
-import { format } from "date-fns";
-import { Clock } from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { Meeting } from "@/types/meeting";
+import { format } from "date-fns";
+import { Clock } from "lucide-react";
 
 interface MeetingInfoCardProps {
   meeting: Meeting;
@@ -12,20 +12,24 @@ export default function MeetingInfoCard({ meeting }: MeetingInfoCardProps) {
   const endTime = format(new Date(meeting.end_time), "h:mm a");
 
   return (
-    <Card className="space-y-2 rounded-2xl border border-blue-200 bg-blue-100 p-5">
+    <Card className="gap-2 rounded-[12px] border border-[#CEE4F1] bg-[#f3f8fc80] px-4 py-6">
       <div className="flex items-center justify-between gap-2">
-        <span className="flex items-center gap-2 text-xs font-semibold">
-          <Clock className="size-4" />
+        <span className="flex items-center gap-2 font-sans text-[14px] leading-5 font-medium tracking-normal text-[#332802]">
+          <Clock className="size-4.5" />
           {startTime}–{endTime}
         </span>
-        <span className="rounded-full bg-[#0A76B9] px-3 py-1 text-xs font-medium text-white">
+        <span className="rounded-full bg-[#0A76B9] px-2 py-1 font-sans text-[9px] leading-[14px] font-medium tracking-normal text-[#F3F8FC]">
           Upcoming
         </span>
       </div>
-      <h3 className="text-lg leading-snug font-medium text-[#06476F]">{meeting.title}</h3>
-      <div className="flex items-center gap-2 text-xs text-blue-900">
-        <span className="font-medium">Host</span>
-        <span className="truncate rounded-full bg-blue-200/70 px-3 py-1 font-medium">
+      <h3 className="mt-1.5 font-sans text-[14px] leading-5 font-medium tracking-normal text-[#06476F]">
+        {meeting.title}
+      </h3>
+      <div className="flex items-center gap-2">
+        <span className="font-sans text-[14px] leading-5 tracking-normal text-[#042F4A]">
+          Host
+        </span>
+        <span className="truncate rounded-full border border-[#E2E8F0] bg-[#CDD6DC] px-2.5 py-1 text-[14px] leading-5 tracking-normal text-[#06476F]">
           {meeting.booked_by}
         </span>
       </div>
